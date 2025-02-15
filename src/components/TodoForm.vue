6d229cafc070b32ccb921203775d67d84ea62d82
NKSL2001 <s2116593@siswa.um.edu.my>
2025-02-15 10:21:31 +0800

    setup form reactive

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,8 +1,8 @@
 <template>
   <form>
-    <input type="text" placeholder="Activity" name="activity" />
-    <input type="number" placeholder="Price" name="price" />
-    <select placeholder="Type" name="type">
+    <input type="text" placeholder="Activity" name="activity" v-model="formData.activity" />
+    <input type="number" placeholder="Price" name="price" v-model="formData.price" />
+    <select placeholder="Type" name="type" v-model="formData.type">
       <option value="education">Education</option>
       <option value="recreational">recreational</option>
       <option value="social">social</option>
@@ -13,10 +13,31 @@
       <option value="music">music</option>
       <option value="busywork">busywork</option>
     </select>
-    <input type="checkbox" name="bookingRequired" />
+    <input type="checkbox" name="bookingRequired" v-model="formData.bookingRequired" />
     <div>
-      <input type="range" name="accessibility" min="0" max="1" step="0.01" />
+      <input
+        type="range"
+        name="accessibility"
+        min="0"
+        max="1"
+        step="0.01"
+        v-model="formData.accessibility"
+      />
     </div>
     <button type="submit">Add</button>
   </form>
 </template>
+
+<script setup lang="ts">
+import { reactive, type Reactive } from 'vue'
+import type { Todo } from '@/interfaces/ITodo'
+
+const formData: Reactive<Todo> = reactive({
+  id: -1,
+  activity: '',
+  price: 0,
+  type: '',
+  bookingRequired: false,
+  accessibility: 0,
+})
+</script>
